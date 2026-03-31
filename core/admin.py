ecf7c1106334c84ad9d221785b29fd1c6d0d8115
Bruno-Braganca-Fernandes <bruno.b.fernandes12@hotmail.com>
2026-03-30 21:43:23 -0300

    feat(core): implement user feed logic and register custom fields in admin

--- core/admin.py
+++ core/admin.py
@@ -2,8 +2,12 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import User, Post, Comment, Like
 
-admin.site.register(User, UserAdmin)
+class CustomUserAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (
+        ('Informações Adicionais (Clone do X)', {'fields': ('bio', 'profile_picture', 'following')}),
+    )
 
+admin.site.register(User, CustomUserAdmin)
 admin.site.register(Post)
 admin.site.register(Comment)
 admin.site.register(Like)
