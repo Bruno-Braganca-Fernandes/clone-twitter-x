c717775cb2231dced9720cd03a379621956543b9
Bruno-Braganca-Fernandes <bruno.b.fernandes12@hotmail.com>
2026-04-14 17:37:18 -0300

    test(api): add user follow/unfollow and comment on post tests

--- core/tests.py
+++ core/tests.py
@@ -36,3 +36,35 @@ class PostInteractionTests(APITestCase):
         self.assertEqual(response_unlike.status_code, status.HTTP_200_OK)
         self.assertFalse(response_unlike.data['liked'])
         self.assertEqual(response_unlike.data['likes_count'], 0)
+
+class UserAndCommentTests(APITestCase):
+    def setUp(self):
+        self.user1 = User.objects.create_user(username='bruno', password='123')
+        self.user2 = User.objects.create_user(username='dafny', password='123')
+        
+        self.client.force_authenticate(user=self.user1)
+        
+        self.post_dafny = Post.objects.create(author=self.user2, content='Post da Dafny')
+
+    def test_comment_on_post(self):
+        url = '/api/comments/'
+        data = {'post': self.post_dafny.id, 'content': 'Ótima postagem!'}
+        response = self.client.post(url, data)
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.data['content'], 'Ótima postagem!')
+        self.assertEqual(response.data['author_username'], 'bruno')
+
+    def test_follow_and_unfollow_user(self):
+        follow_url = f'/api/users/{self.user2.username}/follow/'
+        response_follow = self.client.post(follow_url)
+        
+        self.assertEqual(response_follow.status_code, status.HTTP_200_OK)
+        self.assertEqual(self.user1.following.count(), 1)
+        self.assertEqual(self.user2.followers.count(), 1)
+
+        unfollow_url = f'/api/users/{self.user2.username}/unfollow/'
+        response_unfollow = self.client.post(unfollow_url)
+
+        self.assertEqual(response_unfollow.status_code, status.HTTP_200_OK)
+        self.assertEqual(self.user1.following.count(), 0)
