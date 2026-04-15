45472bf5505afec0e8c1a063a73c19b6b9ad22ad
Bruno-Braganca-Fernandes <bruno.b.fernandes12@hotmail.com>
2026-04-15 19:08:59 -0300

    fix(cors): allow vercel frontend to access django api

--- setup/settings.py
+++ setup/settings.py
@@ -159,6 +159,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "https://twitter-clone-bybruno.vercel.app",
 ]
 
 MEDIA_URL = '/media/'
